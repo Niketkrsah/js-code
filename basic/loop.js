4d3fcb1d6084ca2e152d2f2e2c921eba9e0b4ec4
Niket Kumar <118383214+Niketkrsah@users.noreply.github.com>
2025-05-29 06:17:07 +0000

    container config done

--- basic/loop.js
+++ basic/loop.js
@@ -4,4 +4,95 @@
 // }
 
 
+//  let myarray=["flash","batman","superman"]
+//  let arr=0
+//  while(arr < myarray.length){
+//     console.log(`value is ${myarray[arr]}`);
+//     arr+=1
+//  }
+
+// let score=1  
+// do{
+//  console.log(`scoreis ${score}`);
+//  score++
+// } while(score<=10);
+
+////for of loop
+//["","",""] // string in array
+//[{},{},{}]// objecty in array
+
+
+
+// const arr=[1,2,3,4,5]
+// for (const num of arr){
+//     console.log(num);
+// }
+
+// const greeting="Hello world1"
+// for(const greet of greeting){ // take any name instance of num, greet etc
+//     console.log(`each char is ${greet}`)
+// }
+
+//  const map =new Map()
+//  map.set("IN","India")
+//  map.set("usa","united state of america")
+//  console.log(map);
+// //  for(const key of map){// give array of each
+// //     console.log(key);
+// //  }
+
+
+//  for(const [key,value] of map){
+//    console.log(key,":-",value);
+//  }
+
+// const myobject={
+//     game1:"NFS",
+//     game2:"spiderman",
+//     js: "javascript"
+// }
+//  for (const key in myobject) {
+//         console.log(`${key} shortcut is for ${myobject[key]}`);
+// }
+
+
+// const programming=["js","python","java"]
+// for (const key in programming) {
+//     console.log(programming[key]);
+// }
  
+// const coding=["js","python","java"]
+
+// /// 1st way
+// // coding.forEach(function(iteam){
+// //     console.log(iteam)
+// // })
+
+
+// //// 2nd way
+// // function print(iteam){
+// //     console.log(iteam);
+// // }
+// // coding.forEach(print)
+
+// //3rd way
+// coding.forEach((iteam,indexedDB,arr) =>{
+//     console.log(iteam,indexedDB,arr);
+// })
+
+const mycoding=[{
+    languageName:"javascript",
+    languagefilename:"js"
+},
+{
+    languageName:"python",
+    languagefilename:"py"
+},
+{
+    languageName:"c programing",
+    languagefilename:"c"
+}
+]
+mycoding.forEach((iteam) => {
+    console.log(iteam.languageName)
+})
